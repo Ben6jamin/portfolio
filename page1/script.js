413c6f3d0153209e2d7fa8e234b3eaa9dd00d3c4
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2023-05-24 15:01:06 +0000

    script

---
+++ page1/script.js
@@ -0,0 +1,8 @@
+function scaleCircle(element) {
+    element.style.transform = "scale(1.1)"; // Scale up the circle
+  }
+  
+  function resetCircle(element) {
+    element.style.transform = "scale(1)"; // Reset the circle to its original size
+  }
+  
