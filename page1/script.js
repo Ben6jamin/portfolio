adacbd383d03abda867f1a5a61f5217acf117832
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2023-05-31 13:41:14 +0000

    hard

--- page1/script.js
+++ page1/script.js
@@ -5,4 +5,21 @@ function scaleCircle(element) {
   function resetCircle(element) {
     element.style.transform = "scale(1)"; // Reset the circle to its original size
   }
+  document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
   
+    // Get form values
+    var name = document.getElementsByName('name')[0].value;
+    var email = document.getElementsByName('email')[0].value;
+    var message = document.getElementsByName('message')[0].value;
+  
+    // Prepare email body
+    var emailBody = 'Name: ' + name + '\n';
+    emailBody += 'Email: ' + email + '\n';
+    emailBody += 'Message: ' + message;
+  
+    // You'll need to implement the server-side code to send the email using the emailBody variable.
+  
+    // Reset form
+    document.getElementById('contactForm').reset();
+  });
