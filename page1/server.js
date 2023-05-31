672ecdd64518c60fe3b09a487fc6eb3860822d87
Ben6jamin <113389731+Ben6jamin@users.noreply.github.com>
2023-05-31 14:43:22 +0000

    update

--- page1/server.js
+++ page1/server.js
@@ -43,6 +43,6 @@ app.post('/submit-form', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(5500, () => {
+  console.log('Server running on port 5500');
 });
